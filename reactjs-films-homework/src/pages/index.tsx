ef4b81879f0dc4b725e6f2781ae9dcb2bb2a138b
Denis Gusar <degusar.wot@gmail.com>
2022-09-08 15:49:12 +0300

    refactor: replace page components

--- reactjs-films-homework/src/pages/index.tsx
+++ reactjs-films-homework/src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { ActorProfile } from "./ActorProfile";
-import { Main } from "./Main";
-import { MovieDetails } from "./MovieDetails";
-import { NotFoundPage } from "./NotFoundPage";
+import { ActorProfile } from "./ActorProfile/ActorProfile";
+import { Main } from "./Main/Main";
+import { MovieDetails } from "./MovieDetails/MovieDetails";
+import { NotFoundPage } from "./NotFoundPage/NotFoundPage";
 
 export { ActorProfile, Main, MovieDetails, NotFoundPage };
